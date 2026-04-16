cbd90fd5831f09c6beaadbb5112fa0df67a48c2c
Alexander Öberg <stativetroller7@gmail.com>
2026-04-16 11:13:09 +0200

    Revert "upload alexander branch"
    This reverts commit 422edd5b5b871769d21a213e15dff5cdfd123df1.

--- src/main/java/se/lexicon/exception/DuplicateContactException.java
+++
@@ -1,9 +0,0 @@
-package se.lexicon.exception;
-
-/// Checked exception thrown when a contact with the same name already exists in storage.
-public class DuplicateContactException extends Exception {
-
-    public DuplicateContactException(String message) {
-        super(message);
-    }
-}
