a6dd49cd0dba61c955f946b38ef3aac04f1915da
Kristy Heijenk <ksheijenk@hotmail.com>
2026-04-16 15:10:34 +0200

    Refactor: restored project structure, moved nested project to root, updated packages

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,21 @@
 package se.lexicon;
 
-public class Main {
-    static void main() {
+import controller.ContactController;
+import data.FileCOntactDAOImpl;
+import view.ContactView;
 
+import java.nio.file.Path;
+
+public class Main {
+    static <FileContactDAOImpl> void main(String[] args) {
+
+        FileContactDAOImpl dao = new FIleCOntactDAOImpl(Path.of("contacts.txt"));
+        ContactView view = new ContactView();
+
+        ContactController controller = new ContactController(dao, view);
+        controller.run();
     }
+
 }
+
+// I have to check everyhing as a whole.
---
+++ src/main/java/se/lexicon/view/ContactView.java
@@ -0,0 +1,40 @@
+package se.lexicon.view;
+
+import java.util.List;
+import java.util.Scanner;
+import model.Contact;
+
+// I'm waiting on the Contact class so things will not work yet.
+
+public class ContactView {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getUserInput(String prompt) {
+        System.out.println(prompt + ": ");
+        return scanner.nextLine();
+    }
+
+    public void displayMenu() {
+        System.out.println("\n=== Contact app ===");
+        System.out.println("1. Add Contact");
+        System.out.println("2. View All Contacts");
+        System.out.println("3. Find Contact by Name");
+        System.out.println("0. Exit");
+    }
+
+    public void displayContacts(List<Contact> contacts) {
+        for (Contact c : contacts) {
+            System.out.println(c.getName() + "- " + c.getPhoneNumber());
+
+        }
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void displayError (String message) {
+        System.out.println("ERROR: " + message);
+    }
+}
